f3081e6be649935b18d0e9d1c0b2fb4e660b621c
mbsec <mbsec@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-08-14 21:22:49 +0000

    changed version number
    git-svn-id: https://plugins.svn.wordpress.org/link-checker/tags/1.0.0-beta.2@1221280 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- linkchecker.php
+++ linkchecker.php
@@ -9,7 +9,7 @@
 Plugin Name: Link Checker
 Plugin URI: https://www.marcobeierer.com/wordpress-plugins/link-checker
 Description: An easy to use Link Checker for WordPress to detect broken internal and external links on your website.
-Version: 1.0.0-beta.1
+Version: 1.0.0-beta.2
 Author: Marco Beierer
 Author URI: https://www.marcobeierer.com
 License: GPL v3
